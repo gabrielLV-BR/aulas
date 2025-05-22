aed58f3d79fb1a03d3e9c5618aa6bf65b5a51a7b
Gabriel L. <46785225+gabrielLV-BR@users.noreply.github.com>
2025-05-21 22:01:44 -0300

    Atualizado pra ser menos boçal

--- WIP gophers (ford-fulkerson).c
+++ WIP gophers (ford-fulkerson).c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define ONE ((edge) {1, 1})
+
 typedef struct position_t {
     float x, y;
 } position;
@@ -10,15 +12,14 @@ typedef struct edge_t {
     int capacity, flux;
 } edge;
 
-edge** make_matrix(int width, int height) {
+edge** make_matrix(int n) {
     int i;
     edge **matrix;
 
-    matrix = (edge**) malloc(sizeof(edge*) * height);
+    matrix = (edge**) malloc(sizeof(edge*) * n);
 
-    for (i = 0; i < height; i++) {
-        // talvez errei a ordem do calloc
-        matrix[i] = (edge*) calloc(width, sizeof(edge));
+    for (i = 0; i < n; i++) {
+        matrix[i] = (edge*) calloc(n, sizeof(edge));
     }
 
     return matrix;
@@ -26,21 +27,22 @@ edge** make_matrix(int width, int height) {
 
 position* read_positions(int count) {
     int i;
-    position* positions = (position*) malloc(sizeof(position) * count);
+    position* positions = (position*) calloc(count, sizeof(position));
 
     for (i = 0; i < count; i++) {
-        scanf("%f%f", &positions[i].x, &positions[i].y);
+        scanf("%f %f", &positions[i].x, &positions[i].y);
+        printf("Lido %f %f\n", positions[i].x, positions[i].y);
     }
 
     return positions;
 }
 
-void print_matrix(edge **matrix, int width, int height) {
+void print_matrix(edge **matrix, int n) {
     int i, j;
 
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            printf("(%d, %d)\t", matrix[i][j].capacity, matrix[i][j].flux);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d\t", matrix[i][j].capacity);
         }
         putchar('\n');
     }
@@ -61,23 +63,35 @@ void link_gophers_to_holes(
     int g, h;
     float dist;
 
-    for (g = 1; g <= gopher_count; g++) {
-        for (h = 1; h <= hole_count; h++) {
-            if ((velocity * seconds) >= distance(gophers[g], holes[h])) {
-                matrix[g][h].capacity = 1;
-                matrix[g][h].flux = 1;
+    for (g = 0; g < gopher_count; g++) {
+        for (h = 0; h < hole_count; h++) {
+            dist = distance(gophers[g], holes[h]);
+
+            int gopher = g;
+            int hole = h + gopher_count;
+
+            printf("[%d](%f, %f) - (%f) -> [%d](%f, %f)\n",
+                gopher, gophers[gopher].x, gophers[gopher].y,
+                dist,
+                hole, holes[hole].x, holes[hole].y);
+
+            if ((velocity * seconds) >= dist) {
+                matrix[gopher][hole] = ONE;
+                matrix[hole][gopher] = ONE;
             }
         }
     }
 
+    int origin = gopher_count + hole_count;
+    int target = gopher_count + hole_count + 1;
     // link origin vertex to all gophers
     for (g = 0; g <= gopher_count; g++) {
-        matrix[0][g] = (edge) { 1, 1 };
+        matrix[origin][g] = ONE;
     }
 
     // link all holes to target vertex
     for (h = 0; h <= hole_count; h++) {
-        matrix[h][0] = (edge) { 1, 1 };
+        matrix[h][target] = ONE;
     }
 }
 
@@ -90,12 +104,12 @@ int main() {
     
     edge **matrix;
 
-    scanf("%d%d%f%f", &gopher_count, &hole_count, &seconds, &velocity);
+    scanf("%d %d %f %f", &gopher_count, &hole_count, &seconds, &velocity);
 
     gophers = read_positions(gopher_count);
     holes = read_positions(hole_count);
 
-    matrix = make_matrix(hole_count + 1, gopher_count + 1);
+    matrix = make_matrix(hole_count + gopher_count + 2);
 
     link_gophers_to_holes(
         matrix,
@@ -103,7 +117,7 @@ int main() {
         holes, hole_count,
         seconds, velocity);
 
-    print_matrix(matrix, gopher_count + 1, hole_count + 1);
+    print_matrix(matrix, gopher_count + hole_count + 2);
 
     free(gophers);
     free(holes);
